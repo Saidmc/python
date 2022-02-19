1dad2a8fe22d30192ffe2a01162f0a14c4360e18
Said <said.munoz.1@bbva.com>
2022-02-19 01:26:51 -0600

    Subiendo solo ListasTupasSets.py

---
+++ ejercicios_rapidos/ListasTuplasSets.py
@@ -0,0 +1,15 @@
+Lista = (1, 2, 3, 4, 5, 1, 2)
+Tupla = [1, 2, 3, 4, 5, 1, 2]
+Set = {1, 2, 3, 4, 5, 1, 2}
+
+print("tipo de Lista: " + str(type(Lista)))
+print("tipo de Tupla: " + str(type(Tupla)))
+print("tipo de Set: " + str(type(Set)))
+
+#Lista, es un conjunto de datos homogeneaos o heterogeneos
+#Tupla, es un tipo de lista donde sus elementos no pueden ser modificados, eliminados y se pueden agregar nuevos
+#Set, es un conjunto de datos que no pueden repetirse
+
+print(Lista)
+print(Tupla)
+print(Set)
